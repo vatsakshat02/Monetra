8115ba49eb3ac6d980b86116eed791a179fe9f0a
vatsakshat02 <vatsakshat02@gmail.com>
2026-05-14 17:14:58 +0530

    feat: added User Schema

---
+++ src/models/User.ts
@@ -0,0 +1,72 @@
+import { Document, Schema } from "mongoose";
+import { Interface } from "readline";
+
+export type userRole =
+  | "borrower"
+  | "admin"
+  | "sales"
+  | "sanction"
+  | "disbursement"
+  | "collection";
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  role: string;
+  pan: number;
+  dateOfBirth: Date;
+  monthlySalary: number;
+  employmentMode: "salaried" | "self-employed" | "unemployed";
+  isProfileComplete: boolean;
+  createdAt: Date;
+}
+
+const userSchema = new Schema<IUser>({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: [
+      "borrower",
+      "admin",
+      "sales",
+      "sanction",
+      "disbursement",
+      "collection",
+    ],
+    default: "borrower",
+  },
+  pan: {
+    type: Number,
+    uppercase: true,
+  },
+  dateOfBirth: {
+    type: Date,
+  },
+  monthlySalary: {
+    type: Number,
+  },
+  employmentMode: {
+    type: String,
+    enum: ["salaried" ,"self-employed" , "unemployed"];
+  },
+  isProfileComplete: {
+    type:Boolean,
+    default: false
+  }
+},{timestamps: true});
